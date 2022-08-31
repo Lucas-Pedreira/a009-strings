21ba37102746bfa2fd80d61e291513edecf098cb
Lucas-Pedreira <lucasrpedreira@gmail.com>
2022-08-31 13:55:13 -0300

    ex2feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log (`Antes de remover os espaços a citação tinha ${minhaString.length} caracteres, e após remove-los sobrou ${minhaString.trim().length} caracteres.`)
+
+const novaFrase = minhaString.replaceAll("________","sticioso")
+console.log (novaFrase)
+
+
+
+//const minhaString100.trim()
